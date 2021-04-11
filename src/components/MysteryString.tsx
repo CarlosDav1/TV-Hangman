7d460e84dbec6caa8ee974eb49af00ea4053c7b9
Carlos Gonzalez <carlosdav1@outlook.com>
2021-04-11 06:51:29 -0600

    Added a component that hides the name of the show

--- src/components/MysteryString.tsx
+++ src/components/MysteryString.tsx
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface props{
     name: string
-    language?: string
+}
+
+const HideLetters = (fullString: string): string => {
+    return typeof fullString === 'undefined'? '' : fullString.replace(/[a-z]/ig, ' _');
 }
 
 let MysteryString = ({name}: props) => {
     let [answer, setAnswer] = useState(name);
-    return(<h1 style={{height: "200px"}} onClick={() => console.log(name)}></h1>);
+    let [hiddenString, setHiddenString] = useState('');
+
+    useEffect(() => {
+        setAnswer(name);
+        setHiddenString(HideLetters(name));
+    }, [name]);
+
+    return(<h1 style={{height: "200px"}} onClick={() => console.log(hiddenString)}></h1>);
 }
 
 export default MysteryString;
